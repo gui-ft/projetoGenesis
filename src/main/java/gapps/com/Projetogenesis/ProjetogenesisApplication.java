2861f725d3b2d947d5b4020bec1902bbab347590
Guilherme Freitas <guilhermealexsandro77@gmail.com>
2018-05-14 18:11:48 -0300

    configurações para execução externa

--- src/main/java/gapps/com/Projetogenesis/ProjetogenesisApplication.java
+++ src/main/java/gapps/com/Projetogenesis/ProjetogenesisApplication.java
@@ -2,10 +2,16 @@ package gapps.com.Projetogenesis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjetogenesisApplication {
+public class ProjetogenesisApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjetogenesisApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetogenesisApplication.class, args);
 	}
